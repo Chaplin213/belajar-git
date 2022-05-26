5c5049fbfcd63c13f03460441539c92fd13e0649
Adam dwi Julianto <81203366+adam23145@users.noreply.github.com>
2022-05-26 22:01:59 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: "AIzaSyBM4QQa30WW9Sf_OkRj4cyhHxoERGxeyIY",
+    appId: "1:365021725881:web:ea33d752598df23ff10768",
+    messagingSenderId: "365021725881",
+    projectId: "chatapp-d7c10",
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'chat',
+      theme: ThemeData(
+        primaryColor: Colors.orange[900],
+      ),
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
